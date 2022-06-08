7134ad9a53dba36e99c854974f2bcfc4c212494f
TaLea Carpenter <32846833+TaLeaMonet@users.noreply.github.com>
2022-06-08 16:02:58 -0700

    hide 'other' job role field by default. Create event listener to display or hide 'other' field.

--- interactive-form-v3/js/script.js
+++ interactive-form-v3/js/script.js
@@ -1,8 +1,23 @@
 //Selects the input with the id of name and makes it the focus state on page load. 
-let nameInput = document.getElementById('name');
+const nameInput = document.getElementById('name');
 nameInput.focus();
 nameInput.select();
 
 
+const jobRoleOption = document.getElementById('title');
+//Selects the job option input and the optional field for 'other' job role. 
+const otherJobRole = document.getElementById('other-job-role');
+//Hide the 'other' job role field by default. 
+otherJobRole.type = 'hidden';
+
+//Event listener on the job role options. 'Other' job role option 
+//should stay hidden unless the other option is selected. 
+ jobRoleOption.addEventListener("change", () => {
+    if(jobRoleOption.value === 'other'){
+        otherJobRole.type = 'text';
+    } else {
+        otherJobRole.type = 'hidden';
+    }
+});
 
 
