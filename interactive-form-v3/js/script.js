012c1f3a8e3a9e3bce62ef998dd04f73244c58fc
TaLea Carpenter <32846833+TaLeaMonet@users.noreply.github.com>
2022-06-08 11:38:32 -0700

    make first text field default focus state.

---
+++ interactive-form-v3/js/script.js
@@ -0,0 +1,8 @@
+//Selects the input with the id of main and makes it the focus state on page load. 
+let nameInput = document.getElementById('name');
+nameInput.focus();
+nameInput.select();
+
+
+
+
